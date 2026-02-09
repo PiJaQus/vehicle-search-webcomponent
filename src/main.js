1e03222bafbd929b4c6177f9851c0bb7669962dc
Pijaq <kevinusia@gmail.com>
2026-02-09 12:26:29 +0100

    feat: expose component as webcomponent

--- src/main.js
+++ src/main.js
@@ -2,4 +2,26 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Create new HTML tag.
+// WebComponent
+class VehicleSearchElement extends HTMLElement {
+  connectedCallback() {
+    // avoid mounting twice
+    if (this._mounted) return
+    this._mounted = true
+
+    this._app = createApp(App)
+    this._app.mount(this)
+  }
+
+  disconnectedCallback() {
+    // clean up if element removed
+    if (this._app) {
+      this._app.unmount()
+      this._app = null
+      this._mounted = false
+    }
+  }
+}
+
+customElements.define('digital35-meinfahrzeugshop-suche', VehicleSearch)
